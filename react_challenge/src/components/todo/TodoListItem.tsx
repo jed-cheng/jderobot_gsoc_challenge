93b07cd4a0224c4c08845d1c2d48ec15a7fdfb7d
jedcheng <jedcheng@outlook.com>
2024-03-14 23:20:40 -0400

    Add new components, hooks, and slices

---
+++ react_challenge/src/components/todo/TodoListItem.tsx
@@ -0,0 +1,11 @@
+interface TodoListItemProps {
+    id: number;
+    title: string;
+    completed: boolean;
+}
+
+export default function TodoListItem() {
+    return (
+        <div></div>
+    )
+};
---
+++ react_challenge/src/lib/slices/todo/todoAPI.ts
@@ -0,0 +1,2 @@
+export const fetchTodos = async () => {
+}
