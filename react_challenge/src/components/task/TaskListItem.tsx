06c59ead64f6e459acc20aa1b6956da9ba53d1f7
jedcheng <jedcheng@outlook.com>
2024-03-18 17:58:34 -0400

    Update component styles and add category column

--- react_challenge/src/components/task/TaskListItem.tsx
+++ react_challenge/src/components/task/TaskListItem.tsx
@@ -2,27 +2,43 @@ import { CalendarDays, Circle } from "lucide-react";
 import { Checkbox } from "../ui/checkbox";
 import { Badge } from "../ui/badge";
 import { type Task } from "@/lib/schema";
+import { priorities } from "@/lib/consts";
+import { cn } from "@/lib/utils";
 
 
 export default function TaskListItem(props: Task) {
-    console.log("task props:",props)
+    const priority = priorities.find((p) => p.value === props.priority)
+
     return (
         <div className=" flex items-center px-4 py-2 cursor-pointer min-w-64 max-w-80 min-h-16 border rounded-md hover:bg-primary-foreground">
             <Checkbox className="mr-4"/>
             <div className="mr-4">
-                <span>todo</span>
+                <span>{props.title}</span>
                 <div className=" flex items-center flex-wrap gap-x-2 leading-tight  ">
-                    <span className="">
-                        <CalendarDays size={14} className= " inline mr-1"/> 
-                        <span className=" text-xs"> {new Date().toLocaleDateString()}</span>
-                    </span>
+                    {props.due && (
+                        <span>
+                            <CalendarDays size={14} className= " inline mr-1"/> 
+                            <span className=" text-xs"> {props.due.toLocaleDateString()}</span>
+                        </span>
+                    )}
+
                     <span>
                         <Circle size={10} className=" inline mr-1" />
                         category
                     </span>
                 </div>
             </div>
-            <Badge className=" rounded-full h-5 ml-auto"> priority </Badge>
+            {props.priority && (
+                    <Badge variant={"outline"} className={cn(
+                        priority?.value === "1" && "bg-primary-foreground text-primary-foreground",
+                        priority?.value === "2" && "bg-primary-foreground text-primary-foreground",
+                        priority?.value === "3" && " bg-destructive-foreground text-destructive-foreground",
+                    
+                    )}>
+                        {priority?.icon && <priority.icon size={16} className="inline mr-1" />}
+                        {priority?.label}
+                    </Badge>
+            )}
         </div>
     )
 };
--- react_challenge/src/components/task/colums.tsx
+++ react_challenge/src/components/task/colums.tsx
@@ -3,14 +3,15 @@
 import { Task } from "@/lib/schema"
 import { ColumnDef } from "@tanstack/react-table"
 
-// This type is used to define the shape of our data.
-// You can use a Zod schema here if you want.
-
 
 export const columns: ColumnDef<Task>[] = [
   {
     accessorKey: "title",
     header: "Title",
+  },
+  {
+    accessorKey: "category",
+    header: "Category",
   },
   {
     accessorKey: "priority",
